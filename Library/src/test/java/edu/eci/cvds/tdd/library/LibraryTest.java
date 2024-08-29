b62ce66d281a368cca7cf6622cbee96ba4cb81c2
lab lab <jorge.gamboa-s@mail.escuelaing.edu.co>
2024-08-29 15:56:37 -0500

    Clases de prueba

---
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -0,0 +1,104 @@
+package edu.eci.cvds.tdd.library;
+
+
+import org.junit.jupiter.api.Test;
+
+public class LibraryTest {
+//pasa
+    @Test
+    public void AddanExistingBookSuccessfully(){
+
+
+    }
+    //pasa
+    @Test
+    public void AddNewBookSuccessfully(){
+
+
+    }
+//Nodeberia pasar
+    @Test
+    public void ParameterWithOtherObject(){
+
+
+    }
+    //No deberia pasar
+    @Test
+    public void VerifyBookWithDiferentTitle(){
+
+    }
+    //No deberia PASAR
+    @Test
+    public void VerifyBookWithDiferentAuthor(){
+
+    }
+    //No deberia pasar
+    @Test
+    public void VerifyBookWithDiferentId(){
+
+    }
+//Lean a Book
+    @Test
+    public void AddNewLeanSuccessfully(){
+
+
+    }
+    @Test
+    public void LeanABookNotAvailable(){
+
+
+    }
+    @Test
+    public void VerifyBookWithDiferentNameButSameAuthor(){
+
+
+    }
+
+    @Test
+    public void VerifyParametersNotFound(){
+
+
+    }
+
+    @Test
+    public void VerifyDecrementAmountOfBook(){
+
+
+    }
+
+    @Test
+    public void LoanStatusActive(){
+    }
+//Return Loan
+    @Test
+    public void VerifyReturnedStatusLoan(){
+
+
+    }
+    @Test
+    public void VerifyReturnedBookIncrement(){
+
+    }
+    @Test
+    public void LoanDoesNotExist(){
+
+
+    }
+    @Test
+    public void VerifyCurrentDate(){
+
+
+    }
+    @Test
+    public void VerifyCorrectUser(){
+
+
+    }
+    @Test
+    public void SendABookFromOtherUser(){
+
+
+    }
+    
+
+}
