4f8af112278de066c26c775896861d1571829b6f
JAGBytes <jorge.gamboa-s@mail.escuelaing.edu.co>
2024-08-31 22:37:02 -0500

    añadiendo atributos a la clase de excepcion

--- Library/src/main/java/edu/eci/cvds/tdd/library/LibraryException.java
+++ Library/src/main/java/edu/eci/cvds/tdd/library/LibraryException.java
@@ -3,6 +3,9 @@ package edu.eci.cvds.tdd.library;
 public class LibraryException extends Exception {
   public static final String notUser = "User not found";
   public static final String notBook = "Book not found";
+  public static final String notLoan = "Loan not found";
+  public static final String CannotHaveMultipleBooksAtSameTime = "User cannot have multiple books on loan at the same time.";
+  public static final String BookNotAvailable = "Book not available";
   public LibraryException(String message) {
     super(message);
   }
